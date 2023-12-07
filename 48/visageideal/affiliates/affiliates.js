b7b0943b648a107db00a50ae3ff4804fab92e031
Patryk Siechodnik <p.siechodnik@gmail.com>
2023-12-07 11:04:20 +0100

    deletion of information for affitiates

--- 48/visageideal/affiliates/affiliates.js
+++
@@ -1,8 +0,0 @@
-import './affiliates.scss';
-
-const form = document.getElementById('form');
-const nickname = document.getElementById('nickname');
-const phoneNumber = document.getElementById('phoneNumber');
-const email = document.getElementById('email');
-const skype = document.getElementById('skype');
-const telegram = document.getElementById('telegram');
